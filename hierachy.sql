1fddc4bb3e1388021c262ee10414356d249b4cde
Vankayala Manasa <11239A111@kanchiuniv.ac.in>
2025-11-15 14:48:24 +0530

    Delete hierachy.sql

--- hierachy.sql
+++
@@ -1,5 +0,0 @@
--- Check if the database exists and then drop it
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
-USE testdb;
-CREATE TABLE students (id INT PRIMARY KEY, name VARCHAR(50), age INT);
